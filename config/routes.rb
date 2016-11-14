3df9bc8479eb2d609364db5fe33f00ba8d9bd57d
Wyatt Sullivan <wsull001@ucr.edu>
2016-11-13 17:07:07 -0800

    hidden config

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcom/index'
-  root 'welcom#index'
-
-  resources :devices
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
